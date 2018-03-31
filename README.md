3bdfea3f3a396bb946965f7fc5eb5a307a65f075
jikagq <theo-nec@hotmail.fr>
2018-03-31 19:47:59 +0200

    debut de la structure du soft

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # msp430-ADC-thermal-sensor
 POWER LED thermal regulation for dual-sunblaster project
+
+Monitor temperature from CTN Thermistors fixed on both POWERLED heatsinks.
+MSP430G2553 aquire the temp and determine the PWM to drive fans
+
+also battery monitoring system
+
+TO-DO-LIST:
+- monitor the temp
+- generate PWM to drive fans
+- design electrical board
+- create circuit with lm3915 to monitor battery charge
+
+
+
+Completed tasks:
+- Test power on one led with fan
+
--- therm-dual-sunblaster/main.c
+++ therm-dual-sunblaster/main.c
@@ -1,12 +1,71 @@
 #include <msp430.h> 
-
+#include "ADC.h"
 
 /**
  * main.c
  */
+
+int temp1=20;
+int temp2=20;
+
+void delay (unsigned int ms)
+{
+    volatile unsigned int i, z;
+    i=100;
+    while (i--) {
+        z=ms;
+        while(z--);
+    }
+}
+
+
+int tempconverion(int tensionadc){//converti la valeur lu de l'adc en temperature reel
+    int temp;
+
+    return temp;
+}
+
+void initpwms(void){//initialise les pwm des ventilateurs
+
+
+}
+
+void regulation(int temp1, int temp2){//regle les pwm en fonction des temperatures
+
+
+
+}
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	P1DIR |= (BIT0 | BIT1);// adc
+    P2DIR |= (BIT2 | BIT4);// pwm out
+
+
+
+	ADC_init();
+	initpwms();
+
+
+	while(1){
+
+
+	    ADC_Demarrer_conversion(0);
+	    temp1=tempconverion(ADC_Lire_resultat());
+
+	    ADC_Demarrer_conversion(1);
+	    temp2=tempconverion(ADC_Lire_resultat());
+
+
+
+
+	    regulation(temp1,temp2);
+
+
+	    delay(1000);
+	}
 	
 	return 0;
 }
